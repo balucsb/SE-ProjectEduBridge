31dbbd9a33f90fcf6ac01e347fe7f86690e9abf5
balucsb <beabalucan@gmail.com>
2025-12-09 00:33:39 +0800

    Update signup_parent.php

--- signup_parent.php
+++ signup_parent.php
@@ -2,21 +2,37 @@
 include 'edubridge_db_connect.php';
 $message = "";
 
-if(isset($_POST['create'])) {
+if (isset($_POST['create'])) {
     $email = $_POST['email'];
     $name = $_POST['name'];
     $password = $_POST['password'];
     $confirm = $_POST['confirm'];
 
-    if($password !== $confirm) {
+    if ($password !== $confirm) {
         $message = "Passwords do not match!";
     } else {
-        $parent_code = substr(md5(uniqid()),0,6);
-        $sql = "INSERT INTO Parent (name, email_address, password) VALUES (?, ?, ?)";
+
+        $parent_code = rand(100, 999);
+
+   
+        $check = $conn->prepare("SELECT parent_id FROM Parent WHERE parent_code=?");
+        $check->bind_param("s", $parent_code);
+        $check->execute();
+        $check_result = $check->get_result();
+
+        while ($check_result->num_rows > 0) {
+            $parent_code = rand(100, 999); 
+            $check->bind_param("s", $parent_code);
+            $check->execute();
+            $check_result = $check->get_result();
+        }
+
+        $sql = "INSERT INTO Parent (name, email_address, password, parent_code)
+                VALUES (?, ?, ?, ?)";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("sss", $name, $email, $password);
+        $stmt->bind_param("ssss", $name, $email, $password, $parent_code);
         $stmt->execute();
-        $parent_id = $stmt->insert_id;
+
         header("Location: signup_success.php?type=parent&code=$parent_code");
         exit();
     }
@@ -26,3 +42,21 @@ if(isset($_POST['create'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <title>Parent Sign Up - EduBridge</title>
+    <link rel="stylesheet" href="signupdesign.css">
+</head>
+<body>
+<div class="form-container">
+    <h2>Parent Sign Up</h2>
+    <form method="POST">
+        <input type="email" name="email" placeholder="Enter your email" required><br><br>
+        <input type="text" name="name" placeholder="Enter your name" required><br><br>
+        <input type="password" name="password" placeholder="Enter a password" required><br><br>
+        <input type="password" name="confirm" placeholder="Confirm password" required><br><br>
+        <button type="submit" name="create">Create Account</button>
+    </form>
+    <p style="color:red;"><?php echo $message; ?></p>
+</div>
+</body>
+</html>
