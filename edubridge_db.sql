2e00634afd14a8d29b25956ccbb41a4b33f9e42d
balucsb <beabalucan@gmail.com>
2025-12-09 00:03:07 +0800

    Update edubridge_db.sql

--- edubridge_db.sql
+++ edubridge_db.sql
@@ -5,7 +5,8 @@ CREATE TABLE Parent (
     parent_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email_address VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    parent_code VARCHAR(20) NOT NULL UNIQUE
 );
 
 CREATE TABLE Child (
